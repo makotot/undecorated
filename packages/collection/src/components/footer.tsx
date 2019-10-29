14ac332e6c0a6c4b8bff46a22e996bdf6e26cba9
makotot <makoto.ttn@gmail.com>
2019-10-30 02:11:26 +0900

    update footer

--- packages/collection/src/components/footer.tsx
+++ packages/collection/src/components/footer.tsx
@@ -1,23 +1,35 @@
-import React, { useContext, ReactNode } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import { FiGithub } from 'react-icons/fi'
-import { InlineCentered } from 'undecorated'
+import { Wrapper } from './wrapper'
 
-const StyledFooter = styled(InlineCentered.Block)`
-  padding: 1rem 0;
+const StyledFooter = styled.div`
+  padding: 2rem 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
 
 const Link = styled.a`
   color: #222;
+  text-align: center;
+`
+
+const Text = styled.span`
+  color: #444;
+  font-size: 0.875rem;
 `
 
 const Footer = () => (
   <footer>
-    <StyledFooter>
-      <Link href="https://github.com/makotot/undecorated/" target="_blank" rel="noopener">
-        <FiGithub />
-      </Link>
-    </StyledFooter>
+    <Wrapper>
+      <StyledFooter>
+        <Link href="https://github.com/makotot/undecorated/" target="_blank" rel="noopener">
+          <FiGithub />
+        </Link>
+        &nbsp;<Text>Undecorated is a collection of reusable CSS layout.</Text>
+      </StyledFooter>
+    </Wrapper>
   </footer>
 )
 
--- packages/collection/src/components/main.tsx
+++ packages/collection/src/components/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 
 const StyledMain = styled.main`
-  padding: 2rem 0 1rem;
+  padding: 2rem 0 0;
 `
 
 const Main: React.SFC = ({ children }) => <StyledMain>{ children }</StyledMain>
